3a63fa5e6bcb11c0f1f519308f5eb6cbb5b176fc
Chris Frank <chris@cfrank.org>
2015-07-30 22:11:15 +0300

    Begin tutorial by testing require

---
+++ Examples/todo-tyler/app/App.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var ListContainer = require('./components/ListContainer');
+
+var App = React.createClass({
+	render: function(){
+		return (
+			<p>Hello World!</p>
+		)
+	}
+});
